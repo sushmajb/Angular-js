14160c534cce42554af6b679fcab2451f41ed0da
sushmajb <jb.sushma15@gmail.com>
2016-05-17 17:04:36 +0530

    search  example

---
+++ search/multiplesearch.js
@@ -0,0 +1,14 @@
+ var app = angular.module("myModule",[]);
+
+ var controller = app.controller("myController",function($scope){
+	var employees = [
+	{name:"Ben", gender:"male", salary:12344, city:"Bangalore"},
+	{name:"Sara", gender:"female", salary:144, city:"Mangalore"},
+	{name:"Mark", gender:"male", salary:1234, city:"Mysore"},
+	{name:"Pam", gender:"female", salary:2344, city:"Delhi"},
+	{name:"Todd", gender:"male", salary:234, city:"London"}
+	];
+	
+	$scope.employees = employees;
+	
+});
